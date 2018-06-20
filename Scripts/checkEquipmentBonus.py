a5f2b077a5b62a67dd031e82b7205dfea86105e7
BlizzardOfOzzy <ojd4hh@virginia.edu>
2018-06-19 17:33:00 -0800

    Added checker for invalid ideology references
    Refactored some commonalities out to start making some general checking tools

--- Scripts/checkEquipmentBonus.py
+++ Scripts/checkEquipmentBonus.py
@@ -1,58 +1,46 @@
-from os import listdir
 import time
-from openFile import open_file
+from Scripts.scopeScanner import ScopeScanner
 
 
 def check_equipment_bonus(path, output_file):
     t0 = time.time()
-    subpath = path + '\\common\\ideas'
 
-    for filename in listdir(subpath):
-        current_line = 1
+    subpaths = ['\\common\\ideas']
+    scope = 'equipment_bonus'
 
-        file = open_file(subpath + '\\' + filename)
-        line = file.readline()
+    equipment_bonus_checker = EquipmentBonusChecker(path, subpaths, scope=scope)
+    equipment_bonuses = equipment_bonus_checker.scan_files()
 
-        while line:
-            if "equipment_bonus" in line:
-                in_equipment_bonus_scope = True
-                instant_equals_yes_found = False
-                scope_level = 1
-                line = file.readline()
-                current_line += 1
-
-                while in_equipment_bonus_scope & bool(line):
-                    if instant_equals_yes_found == False:
-                        instant_equals_yes_found = check_for_instant_equals_yes(line)
-                    scope_level += change_in_scope_level(line)
-                    if scope_level == 0:
-                        in_equipment_bonus_scope = False
-                        if not instant_equals_yes_found:
-                            output_file.write("\'instant = yes\' missing from equipment_bonus scope ending at " + str(current_line) + ' in ' + filename + '\n')
-                    line = file.readline()
-                    current_line += 1
-            else:
-                line = file.readline()
-                current_line += 1
-
+    for equipment_bonus in equipment_bonuses:
+        output_file.write("\'instant = yes\' missing from equipment_bonus scope ending at " + str(
+            equipment_bonus.line) + ' in ' + equipment_bonus.filename + '\n')
 
     t0 = time.time() - t0
     print("Time taken for equipment bonus script: " + (t0*1000).__str__() + " ms")
 
 
-def check_for_instant_equals_yes(line):
-    instant_equals_yes_found = False
-    for string in ['instant = yes', 'instant=yes', 'instant =yes', 'instant= yes']:
-        if string in line:
-            instant_equals_yes_found = True
-    return instant_equals_yes_found
+class EquipmentBonus:
+
+    def __init__(self, line_number, filename):
+        self.line = line_number
+        self.filename = filename
 
 
-def change_in_scope_level(line):
-    change = 0
-    for character in line:
-        if character == '{':
-            change += 1
-        elif character == '}':
-            change += -1
-    return change
+class EquipmentBonusChecker(ScopeScanner):
+
+    def initialize_additional_variables(self):
+        self.equipment_bonuses = []
+
+    def actions_on_scope_start(self):
+        self.instant_found = False
+
+    def actions_in_scope(self, line):
+        if not self.instant_found:
+            self.instant_found = 'instant' in line
+
+    def actions_on_end_of_scope(self, current_line, filename):
+        flagged_equipment_bonus = EquipmentBonus(current_line, filename)
+        self.equipment_bonuses += [flagged_equipment_bonus]
+
+    def return_outputs(self):
+        return self.equipment_bonuses
