8d1acb0c5ff1c027f49cb893efd70694ba71bd17
BlizzardOfOzzy <ojd4hh@virginia.edu>
2018-08-17 02:04:24 -0400

    updated main test files

--- Scripts/checkIdeologies.py
+++ Scripts/checkIdeologies.py
@@ -1,6 +1,8 @@
 import os
 
 from Scripts.generateScopes import generate_scopes
+from Scripts.generateScopeIndicesByType import generate_scope_indices_by_type
+from Scripts.generateTopLevelScopeIndices import generate_top_level_scope_indices
 from Scripts.openFile import open_file
 from Scripts.scope import Scope
 from Scripts.timedFunction import timed_function
@@ -12,7 +14,7 @@ def check_ideologies(path, output_file):
 
     for reference in find_next_reference_to_ideologies(path):
         if reference.body not in ideology_names:
-            output_file.write("Ideology " + reference.body + " not defined at " + str(reference.starting_line) + ' in ' + reference.filename + '\n')
+            output_file.write("Ideology " + reference.body + " not defined at " + str(reference.index) + ' in ' + reference.filename + '\n')
 
 
 def find_ideology_names_in_path(path):
@@ -22,27 +24,19 @@ def find_ideology_names_in_path(path):
     return ideology_names
 
 
-def find_ideology_name(ideology):
-    body = ideology.body.strip(' \n\t\r')
-    words = body.split(' ')
-    ideology_name = words[0]
-    return ideology_name
-
-
 def find_next_reference_to_ideologies(path):
     subpaths = ['\\common', '\\events', '\\history']
     scope = 'has_government'
     for subpath in subpaths:
-        for dirpath, dirs, filename in os.walk(path + subpath):
-            string = open_file(dirpath + filename).read()
-            for index, reference in ScopeExtractorByType(scope).get_next_scope(string):
-                body_after_has_government = reference[reference.index('has_government') + 17:]
-                reference_name = body_after_has_government.split(' ')[0].strip('\n\t\r}')
-                yield Scope(dirpath + filename, index, reference_name)
+        for dirpath, dirs, filenames in os.walk(path + subpath):
+            for filename in filenames:
+                string = open_file(dirpath + filename).read()
+                for index, name, reference in generate_scopes(string, generate_scope_indices_by_type, scope):
+                    yield Scope(dirpath + filename, index, reference)
 
 
 def find_next_ideology_name(ideology_scope):
-    for starting_index, name, body in generate_scopes(ideology_scope.body):
+    for starting_index, name, body in generate_scopes(ideology_scope.body, generate_top_level_scope_indices):
         starting_index += ideology_scope.index - 1
         yield name
 
@@ -54,5 +48,5 @@ def find_next_ideology_scope(path):
 
     for filename in os.walk(full_path):
         string = open_file(filename).read()
-        for start_line, body in ScopeExtractor(scope).get_next_scope(string):
+        for start_line, name, body in generate_scopes(string, generate_scope_indices_by_type, scope):
             yield Scope(filename, start_line, body)
